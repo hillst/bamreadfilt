be64db1a1890e88f80b4e340dc306f7364b2136c
Steven Kothen-Hill <hill2steve@gmail.com>
2018-01-16 12:08:18 -0500

    very very alpha version is now ready!!! future versions should have filtering components (bq mapq?).
    Given a bam and vcf, bamreadfilt returns all reads which contain the annotated vcf.
    
    given: bam, vcf
    recieve: out.bam
    
    timing notes: appears to be slightly faster than the naive version
    correctness notes: doesnt seem to run into the same fucking bug that the pysam version did!
    
    other notes: need to start implementing tests and simple cases in bams that we can work from.

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,24 @@
+#![feature(test)]
+extern crate test;
+extern crate byteorder;
+extern crate rust_htslib;
+
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
-
-/*
-    usecase:
-    You have a bam and you want it only to contain reads from VCF
-    With no uses this is: "get all reads that contain an annotated variant in [VCF]"
-
-    usecase advanced:
-    Get all annotated variants that meet criteria:
-        - bq < 20
-        - mq < 20
-        - mrbq < 40
-        - READ ORIENTATION > 1
-        - NOT IN SOFTMASK
-        - fragment size ?
-        - no insertions
-        - no deletions ...
-        - include readpairs
-        etc
-
-    programmable is better
-
-    Functions and structs:
+use rust_htslib::bam;
+use rust_htslib::bam::Read;
+use rust_htslib::bam::record::CigarString;
+use rust_htslib::bcf;
+use std::path::Path;
 
 
-    Run! --
-        iterate through bam
-            apply filtering rules to relevant variants 
-
-
-    bcf::mod::Reader::from_path
-
-        test this on vcf and see if it works okay. if not we can stick to bcfs i guess.
- */
 
 
 pub struct Config {
-    pub query: String,
-    pub filename: String,
+    pub bam: String,
+    pub vcf:String,
 }
 
 impl Config {
@@ -47,23 +26,207 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments"); // why did I need return + statement here instead of just expr
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let bam = args[1].clone();
+        let vcf = args[2].clone();
+        Ok(Config { bam, vcf})
         
     }
 }
+
+
+#[derive(Copy,Clone)]
+pub struct VCFRecord {
+    pub chrm: u32,
+    pub pos: u32,
+    pub ref_b: u8,
+    pub alt_b: u8, 
+}
+
+impl VCFRecord {
+    pub fn new(record: &bcf::Record) -> VCFRecord {
+        let chrm = match record.rid(){
+            Some(val) => val,
+            None => panic!("Error, missing reference ID"),
+        };
+        let pos = record.pos();
+        let alleles = record.alleles(); // Vec<&[u8]> signature
+        if alleles.len() > 2 {
+            // this shold produce a recoverable error but uh I guess we wont worry about that for now
+            panic!("error, detected non-diploid site");
+        } else {
+            let ref_b = alleles[0][0].clone();
+            let alt_b = alleles[1][0].clone();
+            VCFRecord { chrm: chrm, pos : pos, ref_b: ref_b, alt_b: alt_b } 
+        }
+
+    }
+
+}
+
+
+pub struct BAMVCFRecord<'a> {
+    //something about lifetimes!!! how do we access our current 'thing'
+    _bam: &'a mut bam::IndexedReader,
+    _vcf: Vec<bcf::Record>, 
+    vcf_head: Option< VCFRecord >, //lol why is this option again? because in the last instance we want to trash it?
+    _EOL: bool,
+}
+
+
+impl <'a > BAMVCFRecord<'a> {
+    // two iteration rules
+    //     no reads should be handled by the iterator, constructor should simply setup the start of our vcf
+    // one: if there are no reads, get the next vcf entry and fetch reads (and check if they exist)
+    // two: if there is a vcf entry and reads return the next read 
+    pub fn new(bam_reader: &'a mut bam::IndexedReader, mut vcf_reader: Vec<bcf::Record>) -> BAMVCFRecord <'a> { // if i use a reference will i need a lifetime?
+        //get first vcf entry
+        //set iterator head using fetch
+        let vcf_head = match vcf_reader.pop(){
+            Some(entry) => VCFRecord::new(&entry),
+            None => panic!("No entries in VCF vector."), // is this really necessary? it is nice to exit first..
+        };
+
+        bam_reader.fetch(vcf_head.chrm, vcf_head.pos, vcf_head.pos + 1);     //make sure this is right
+        BAMVCFRecord { _bam: bam_reader, _vcf: vcf_reader, vcf_head: Some(vcf_head), _EOL: false }
+    }
+
+
+    fn _advancevcf_head(&mut self) {
+        let new_head = match self._vcf.pop(){
+            Some(entry) => {
+                let rid = match entry.rid(){
+                    Some(value) => value,
+                    None => panic!("Invalid reference id in VCF"),
+                };
+                //update bam pointer
+                self._bam.fetch(rid, entry.pos(), entry.pos() + 1);
+                Some(VCFRecord::new(&entry))
+            },
+            None => {
+                None  // if the top of our vcf thing is ever head we know to exit!
+            },
+        };
+        self.vcf_head = new_head;
+    }
+
+
+
+}
+
+
+fn _contains_variant(bam_read: &bam::record::Record, vcf: &VCFRecord) -> Option<u32> {
+    /// Returns the position in bam_read that contains vcf_entry. Otherwise returns None.
+
+    /*
+     * bam.pos, for entry (something?) in bam, walk matched bases until pos = vcf.pos, 
+     *   return if alt = vcf.alt, otherwise None
+     * vcf.pos
+     *
+     */ 
+
+
+    None
+
+}
+
+
+impl <'a> Iterator for BAMVCFRecord<'a> {
+    /// iterator that seeks through a bam in the order resolved by a vcf record
+
+
+    /*
+     * The next thing this piece of code needs to do is....
+     *      either figure out how to dervice copy/clone traits for bcf::record
+     *        or manually parse and create our own vcf struct (i personally like this option more since we want to use this stuff downstream)
+     */
+    type Item = (bam::record::Record, VCFRecord);
+
+    fn next(&mut self) -> Option<(bam::record::Record, VCFRecord)>{ //eventually we are going to make this a fancier struct
+        loop {
+            match self.vcf_head {
+                None => return None,
+                Some(vcf_entry) => {
+                    match self._bam.records().next() {
+                        None => { self._advancevcf_head(); },
+                        Some(val) => { 
+                            match val { 
+                                    Ok(val) => {
+                                        // we are denoting position in the read as pir
+                                        // some option for including variants that would occur in softmasked regions.
+                                        let pir = match val.cigar().read_pos(vcf_entry.pos, false, false).expect("Error parsing cigar string"){
+                                            Some(val) => val as usize,
+                                            //None means this read didnt have the annotated variant!
+                                            None => {continue;}
+                                        }; 
+
+                                        if val.seq()[pir] == vcf_entry.alt_b {
+                                            return Some((val, vcf_entry.clone())); 
+                                        } else{
+                                            continue;
+                                        }
+                                    }, //it may be wise to repack these with all relevant information in addition to bam
+                                    Err(val) => panic!("Error reading bam, godspeed."),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 pub fn run() {
     println!("{}", "hello world");
 }
 
 
-mod test {
+#[cfg(test)]
+mod tests {
     use super::*;
-
+    use test::Bencher;
     #[test]
     fn one_result() {
 
     }
+
+    #[bench]
+    fn iterate_bamvcf(b: &mut Bencher) {
+        // we are supposed to provide a closure to b.iter() that does one step of 'work'
+
+        println!("Starting benchmark test:");
+        use rust_htslib::bam;
+        use rust_htslib::bam::record::CigarString;
+        use std::path::Path;
+
+        //prep bam
+        let path = Path::new("test/test.bam");
+        let mut bam = bam::IndexedReader::from_path(path).ok().expect("Error opening bam.");
+
+        //prep vcf
+        use rust_htslib::bcf::Reader;
+        let vcf_path = Path::new("test/test.vcf");
+        let mut vcf_itr = Reader::from_path(vcf_path).expect("cannot open vcf"); //originally said .ok().unwrap() which seems straight stupid
+        let mut vcf_list: Vec<rust_htslib::bcf::Record> = vec![];
+        for entry in vcf_itr.records() {
+            vcf_list.push(entry.ok().unwrap());
+        }
+
+        //prep iterator
+        use rust_htslib::bam::record::Cigar;
+        use rust_htslib::bam::Read;
+        let mut bamvcfrecord = BAMVCFRecord::new(&mut bam, vcf_list);
+
+        b.iter( || {
+            match bamvcfrecord.next(){
+                Some((item, vcf)) => {
+                    item.cigar().read_pos(vcf.pos, false, false)
+                },
+                None => {return;},
+            };
+        });
+
+
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 extern crate bamreadfilt;
+extern crate byteorder;
 extern crate rust_htslib;
 
-
 use std::env;
+use std::str;
 use std::process;
 use bamreadfilt::Config;
-use std::thread;
-use std::time::Duration;
+use bamreadfilt::BAMVCFRecord;
+//use std::thread;
+//use std::time::Duration;
+//use byteorder::BigEndian;
+//use byteorder::ReadBytesExt;
+
+
+/*
+   IMPORTANT!!!!
+
+ * Another thing to consider:
+ * We have pileup capabilities within rust (ie given a set of reads at an alignment pileup variants  or some shit)
+ *   we could look into using this to skip mpileup directly or to rewrite it using our tool.
+ *   but that sounds hard -- consider it! benchmark it!
+
+ * the next logical thing to do is see if we can take a bam, return it as an iterator (if it isnt already), and then
+ *  write that bam to disk.
+ * 
+ * the EXTRA information we will ultimately need is: base/ref at position of variation, qual at position of variation, cigar at position of variation, mate pair?
+ */
 
 
 fn main() {
@@ -17,31 +36,68 @@ fn main() {
              println!("Problem parsing arguments: {}", err);
              process::exit(1);
     });
+    parse_bam(&config.bam, &config.vcf);
 
+    // cool so we can successfully parse the AD field now. what do we have left to do with this part?
+    //VCF gives us locations to go look for sites in the bam. next up is actually parsing the bam.
+}
+
+fn parse_bam(bamname: &str, vcf: &str){ 
+    /*
+     * Accept a bamname, set of vcf sites and their alt-refs (just in a form that we can filter for quickly, maybe even a struct)
+     *    returns an iterator of parsed bam entries that are contained in vcfinfo
+     *   
+     *    requirements:
+     *        should be able to apply filers onto said iterator as defined in our use cases
+     *        should be easy to convert back into a bam protobuf or into a tfrecord protobuf
+     */
+
+    use rust_htslib::bam;
+    use rust_htslib::bam::record::CigarString;
     use std::path::Path;
+    let path = Path::new(bamname);
+    let mut bam = bam::IndexedReader::from_path(path).ok().expect("Error opening bam.");
+
+    let head = bam::Header::from_template(bam.header()); // wonder if i gotta clone dis;
+    let mut out = bam::Writer::from_path("test/out.bam", &head).expect("Error opening bam for writing.");
+
+
+    let tid = 0;
+
+    let beg: u32 = 10000;
+    let end: u32 = 10075;
+
+
+    // parse vcf -> vec
     use rust_htslib::bcf::Reader;
-    let path = Path::new("test.vcf");
-    let mut vcf_reader = Reader::from_path(path).ok().unwrap();
+    let vcf_path = Path::new(vcf);
+    let mut vcf_itr = Reader::from_path(vcf_path).expect("cannot open vcf"); //originally said .ok().unwrap() which seems straight stupid
+    let mut vcf_list: Vec<rust_htslib::bcf::Record> = vec![];
+    for entry in vcf_itr.records() {
+        // TODO turn this into a match and explicitly handle failure cases (by exiting?)
+        vcf_list.push(entry.ok().unwrap());
+    }
+
+    // should probably figure out how to turn these into tests
+    println!("hello? guys?");
+    use rust_htslib::bam::record::Cigar;
+    use rust_htslib::bam::Read;
 
 
-    for rec in vcf_reader.records(){
-        let mut record = rec.ok().expect("Error reading record.");
-        let allele_depth  : () = record.info(b"AD").string().unwrap().unwrap();
-        println!("{:?}", allele_depth);
+    let mut bamvcfrecord = BAMVCFRecord::new(&mut bam, vcf_list);
 
-        //what can i do with this type?
-
-
+    // we constructed bamvcfrecord to only allow things that contained variants!
+    for (i, (item, vcf)) in bamvcfrecord.enumerate() {
+        // all you need is read_pos (??? yay)
+        if i % 1000 == 0 {
+            println!("{} reads processed.", i);
+        }
+        let ref_pos = item.cigar().read_pos(vcf.pos, false, false);
+        //ultimately we will still have to do soething fancy here.
+        out.write(&item).unwrap();
     }
 
 
-    //primary difference between this and unwrap_or_else is that since we return () in the Okay case 
-    //      we have nothing we need to unwrap
-
-    //if let Err(e) = minigrep::run(config){ // bc we return a Result type, we explicitly handle the Err case (we also could handle the () -- aka unit -- case )
-    //    println!("Application error: {}", e);
-    //    process::exit(1);
-
-    //} 
 }
+
 
