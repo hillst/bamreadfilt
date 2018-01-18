89473cfd5b97c623b5594b6f80378d025306be82
Steven Kothen-Hill <hill2steve@gmail.com>
2018-01-18 12:36:02 -0500

    Added statistic file writing (defaults to stats.txt... no arguments available) Added a completely implemented CLI (including output to stdout instead of to disk) Added read_count and frag_size (when paired) to our statistics.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,104 @@
 #![feature(test)]
+#![feature(rand)]
+
 extern crate test;
+extern crate rand;
 extern crate byteorder;
 extern crate rust_htslib;
+extern crate argparse;
 
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-
+use argparse::{ArgumentParser, StoreTrue, Store, StoreFalse};
 use rust_htslib::bam;
 use rust_htslib::bam::Read;
-use rust_htslib::bam::record::CigarString;
 use rust_htslib::bcf;
-use std::path::Path;
 
-
-
-
+#[derive(Debug)]
 pub struct Config {
-    pub bam: String,
-    pub vcf:String,
+    pub verbose: bool,
+    pub bam_filename: String , 
+    pub vcf_filename: String , 
+    pub mapq: u8,
+    pub vbq: u8, 
+    pub mrbq: f32, 
+    pub min_pir: usize,
+    pub max_pir: usize,
+    pub use_stdout: bool,
+    pub stats_before: bool,
+    pub stats_after: bool, 
+    pub bam_out_filename: String,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments"); // why did I need return + statement here instead of just expr
+    pub fn new() -> Config {
+
+        let mut verbose = false;
+        let mut bam_filename = "".to_string();
+        let mut vcf_filename = "".to_string(); 
+        let mut mapq = 0;
+        let mut vbq = 0;
+        let mut mrbq = 0.;
+        let mut min_pir = 0;
+        let mut max_pir = 10000000;
+        let mut use_stdout: bool = false;
+        let mut stats_before: bool = true;
+        let mut stats_after: bool = true;
+        let mut bam_out_filename = "out.bam".to_string(); 
+        let mut conf = Config { verbose, bam_filename, vcf_filename, mapq, vbq, mrbq, min_pir, max_pir, use_stdout, stats_before, stats_after, bam_out_filename };
+
+        {  // this block limits scope of borrows by ap.refer() method
+            let mut ap = ArgumentParser::new();
+            ap.set_description("Welcome to bamreadfilt. This is a tool for filtering bams which contain variants. In the most simple use case, bamreadfilt simply returns
+                               a new bam of reads that only contain known variants from a provided vcf. In addition, bamreadfilt provides a simple interface for gathering
+                               statistics and filtering read by a variety of read-specific features.");
+            ap.refer(&mut conf.verbose)
+                .add_option(&["-v", "--verbose"], StoreTrue,
+                "Be verbose");
+            ap.refer(&mut conf.bam_filename)
+                .add_option(&["--bam", "-b"], Store,
+                "Filename of indexed bam for reading.")
+                .required();
+            ap.refer(&mut conf.vcf_filename)
+                .add_option(&["--vcf"], Store,
+                "Filename of vcf for reading.")
+                .required();
+            ap.refer(&mut conf.use_stdout)
+                .add_option(&["--stdout", "-s", "-1"], StoreTrue,
+                "Write bam to stdout insetead of to a file (to sort, for instance).");
+            ap.refer(&mut conf.stats_before)
+                .add_option(&["--stats_before"], StoreTrue,
+                "Collect filter statistics before filtering.");
+            ap.refer(&mut conf.stats_after)
+                .add_option(&["--stats_after"], StoreTrue,
+                "Collect filter statistics for reads after filtering.");
+            //filters
+            ap.refer(&mut conf.mapq)
+                .add_option(&["--mapq"], Store,
+                "Minimum MAPQ to keep read");
+            ap.refer(&mut conf.vbq)
+                .add_option(&["--vbq"], Store,
+                "Minimum base-quality (Phred 33) score at the position of the variant to include a read.");
+            ap.refer(&mut conf.mrbq)
+                .add_option(&["--mrbq"], Store,
+                "Minimum read base quality to include a read. Computes the mean of the full read, not just the mapped portion of the read.");
+            ap.refer(&mut conf.min_pir)
+            .add_option(&["--min_pir"], Store,
+            "Minimum position of the variant in the read to include the read.");
+            ap.refer(&mut conf.max_pir)
+                .add_option(&["--max_pir"], Store,
+                "Maximum position of the variant in the read to include the read.");
+            ap.parse_args_or_exit();
         }
-        let bam = args[1].clone();
-        let vcf = args[2].clone();
-        Ok(Config { bam, vcf})
-        
+        conf
+
     }
 }
 
+
+
+
+
 /// Simple structure for capturing a current VCF entry (removes the INFO field effectively)
-#[derive(Copy,Clone)]
+#[derive(Copy,Clone,PartialEq,Eq,Hash)]
 pub struct VCFRecord {
     pub chrm: u32,
     pub pos: u32,
@@ -86,7 +150,8 @@ impl <'a > BAMVCFRecord<'a> {
             None => panic!("No entries in VCF vector."), // is this really necessary? it is nice to exit first..
         };
 
-        bam_reader.fetch(vcf_head.chrm, vcf_head.pos, vcf_head.pos + 1);     //make sure this is right
+        bam_reader.fetch(vcf_head.chrm, vcf_head.pos, vcf_head.pos + 1).expect("Error fetching entry in bam -- check your bam for corruption.");     //make sure this is right
+               
         BAMVCFRecord { _bam: bam_reader, _vcf: vcf_reader, vcf_head: Some(vcf_head) }
     }
 
@@ -101,7 +166,7 @@ impl <'a > BAMVCFRecord<'a> {
                     None => panic!("Invalid reference id in VCF"),
                 };
                 //update bam pointer
-                self._bam.fetch(rid, entry.pos(), entry.pos() + 1);
+                self._bam.fetch(rid, entry.pos(), entry.pos() + 1).expect("Error fetching entry in bam -- check your bam for corruption.");
                 Some(VCFRecord::new(&entry))
             },
             None => {
@@ -136,7 +201,7 @@ impl <'a> Iterator for BAMVCFRecord<'a> {
                                         // some option for including variants that would occur in softmasked regions.
                                         let pir = match val.cigar().read_pos(vcf_entry.pos, include_softclips, include_dels).expect("Error parsing cigar string"){
                                             Some(val) => val as usize,
-                                            None => {continue;}
+                                            None => { continue; }
                                         }; 
 
                                         // lol this is our default filter, i wonder if we can apply this somewhere else
@@ -148,7 +213,7 @@ impl <'a> Iterator for BAMVCFRecord<'a> {
                                             continue;
                                         }
                                     }, //it may be wise to repack these with all relevant information in addition to bam
-                                    Err(val) => panic!("Error reading bam, godspeed."),
+                                    Err(_) => panic!("Error reading bam, godspeed."),
                             }
                         }
                     }
@@ -158,6 +223,40 @@ impl <'a> Iterator for BAMVCFRecord<'a> {
     }
 }
 
+pub fn lofreq_shitty() {
+    /* How shitty is lofreq?
+     *
+     * (MRD)
+     * Of all sites called by lofreq in the tumor (CA samples and cfDNA),
+     *  how many (stats) were then found in the plasma? Of how many found in the plasma are noise?
+     *
+     *
+     *
+     *  we define noise as anything that fails a filter
+     *
+     *
+     *
+
+     list_of_ca_samples = [];
+     for bam, vcf in ca_samples
+         let bamvcf = bamvcfrecord(bam, vcf);
+
+         // update_stats(x)
+         //     i would like to collect the number of site found and the number of reads associated with each site
+
+         bamvcf.map( |x| { stat_holder_before.update_stats(x) }
+                    )
+               .filter( |x| myfilters(x))
+               .filter( .. )
+               .filter( .. )
+               .map(stat_holder_after.update_stats(x))
+
+               bamreadfilt filtstats bams vcfs
+    */ 
+
+
+}
+
 /// mrbq returns the mean base quality of the passed `record`
 pub fn mrbq(record: &bam::Record) -> f32 {
     let res: u32 = record.qual().into_iter().map( |&x| x as u32 ).sum(); 
@@ -165,27 +264,215 @@ pub fn mrbq(record: &bam::Record) -> f32 {
 }
 
 
-pub fn run() {
-    println!("{}", "hello world");
+/// site_stats computes all stats of interest for a given dataset.
+///     each metrics has a mean/variance
+///     the counts are raw counts (hopefully we dont run out of memory!)
+/// there is also a use-case where we may want site-specific statistics. But we have not addressed that :(
+#[derive(Debug,Clone,Copy)]
+pub struct SiteStats{ 
+    //worthwhile to add something like median?
+    //softmasking, mismatches, insertions?
+
+    // getting number of sites not super intuitive, probably should be its own stat?
+    //      just a collection of sites? does it need its own type? we really just want a 'set-like' object. 
+
+    pub num_sites: u64, 
+    pub num_reads: u64, 
+    pub mrbq: RunningAverage,
+    pub vbq: RunningAverage,
+    pub mapq: RunningAverage,
+    pub pir: RunningAverage,   
+    pub duplicated: RunningAverage,
+    pub paired: RunningAverage,
+    pub frag_size: RunningAverage,
+}
+
+//usage is still map, filter, filter, map
+//so ultimately we are 'mapping' chained update calls
+
+impl SiteStats{
+
+    pub fn new() -> SiteStats {
+        return SiteStats { 
+                    num_sites: 0, num_reads: 0, mrbq: RunningAverage::new(), vbq: RunningAverage::new(),
+                    mapq: RunningAverage::new(), pir: RunningAverage::new(), duplicated: RunningAverage::new(),
+                    paired: RunningAverage::new(), frag_size: RunningAverage::new(),
+               }
+    }
+
+    //for (i, (item, _vcf, _pir)) in bamvcfrecord
+    pub fn collect_stats_stream(&mut self, item: &bam::Record, vcf: &VCFRecord, pir: usize) {
+        self.num_reads += 1; //if unique..... check to make sure this is the same as the number of reads we get back.
+        self.mrbq.update(mrbq(item) as f64);
+        self.vbq.update( item.qual()[pir] as f64);
+        self.mapq.update( item.mapq() as f64);
+        self.pir.update( pir as f64 );
+        self.duplicated.update( item.is_duplicate() as u32 as f64 );
+        self.paired.update( item.is_paired() as u32 as f64 );
+        if item.is_paired() {
+            self.frag_size.update( item.insert_size().abs() as f64 );
+        }
+    }
+}
+
+#[derive(Debug,Clone,Copy)]
+pub struct RunningAverage {
+    count: u64,
+    mean: f64,
+    m2: f64,
+}
+
+/// computes the moments online of a stream. 
+/// # example
+/// 
+/// let mut stats = SiteStats::new();
+/// stuff.map( | read, vcf, pir | { stats.mapq.update(read.mapq)} 
+///     ...
+///     .collect()
+/// let (mapq_mean, mapq_var) = stats.mapq.finalize();
+/// 
+impl RunningAverage {
+    pub fn new() -> RunningAverage {
+        return RunningAverage{ count:0, mean:0.0, m2:0.0 }        
+    }
+
+    /// Updates the running average -- interestingly, we might want a generic type for this (int vs floats?)
+    /// algorithm taken from wikipedia page, the numerically stable version was selection to remove subtraction of very small numbers (precision issue)
+    pub fn update(&mut self, new_value: f64){
+        self.count += 1; //this guarantees count is never 0 correct?
+        let delta = new_value as f64 - self.mean;              // -1
+        self.mean = self.mean + (delta / self.count as f64); 
+        let delta2 = new_value as f64 - self.mean;             
+        self.m2 = self.m2 + delta * delta2;                    
+
+    }
+
+    /// returns the mean and variance for this running average. if the number of samples is less than 2 (ie no variance) then we return None.
+    ///     use this function when you are done updating the struct :)
+    pub fn finalize(&self) -> Option<(f64, f64)>{
+        if self.count < 2 {
+            None
+        } else {
+            let variance = self.m2 / (self.count) as f64; // (self.count - 1 was here before...)
+            return Some((self.mean, variance))
+        }
+    }
+
+}
+
+pub fn tuple_to_csv(tuple: (f64, f64)) -> String{
+    format!("{},{}", tuple.0, tuple.1)
+
 }
 
 
 #[cfg(test)]
 mod tests {
+    //need to come up with a test suite for our main functionality (that or its all encapsulated in the library we are using)
+    //      we dont actually do much that could be 'buggy' since there isnt math or anything occuring.
+    //      could/should just make sure we correctly get 'None' and panics when we should ?
     use super::*;
     use test::Bencher;
+
+    /// tests the range ( 1, 6 )
     #[test]
-    fn one_result() {
+    fn running_average_update_simple() {
+        let mut ra = RunningAverage::new();
+        ra.update(2.);
+        assert_eq!(ra.mean, 2.0);
+        ra.update(1.); 
+        assert_eq!(ra.mean, 1.5);
+        ra.update(3.);
+        assert_eq!(ra.mean, 2.0);
+        ra.update(4.); 
+        assert_eq!(ra.mean, 2.5);
+        ra.update(5.); 
+        let (mean, var) = match ra.finalize(){
+            Some((m, v)) => (m, v),
+            None => {panic!("error, got None type from finalize during testing");}
+        };
+        println!("mean: {}, var: {}", mean, var);
+        assert_eq!(mean, 3.0);
+        assert_eq!(var, 2.0);
+    }
+
+    #[test]
+    fn running_average_update_rand() {
+        use rand::Rng;
+
+        let mut ra = RunningAverage::new();
+        let mut rng = rand::thread_rng();
+        for i in 0..1000000 {
+            ra.update(rng.gen::<u32>() as f64);
+        }
+        let (mean, var) = match ra.finalize(){
+            Some((m, v)) => (m, v),
+            None => {panic!("error, got None type from finalize during testing");}
+        };
+
+        assert_ne!(mean, 0.);
+        assert_ne!(var, 0.);
+    }
+
+    #[test]
+    #[should_panic]
+    fn running_average_update_empty() {
+        let mut ra = RunningAverage::new();
+        let (mean, var) = match ra.finalize(){
+            Some((m, v)) => (m, v),
+            None => {panic!("error, got None type from finalize during testing");}
+        };
+    }
+
+    #[test]
+    fn running_average_update_constant() {
+        let mut ra = RunningAverage::new();
+        ra.update(2.);
+        assert_eq!(ra.mean, 2.0);
+        ra.update(2.); 
+        assert_eq!(ra.mean, 2.0);
+        ra.update(2.);
+        assert_eq!(ra.mean, 2.0);
+        ra.update(2.); 
+        assert_eq!(ra.mean, 2.0);
+        ra.update(2.); 
+        let (mean, var) = match ra.finalize(){
+            Some((m, v)) => (m, v),
+            None => {panic!("error, got None type from finalize during testing");}
+        };
+        println!("mean: {}, var: {}", mean, var);
+        assert_eq!(mean, 2.0);
+        assert_eq!(var, 0.0);
 
     }
 
+
+    #[test]
+    fn stress_running_average_update() {
+        let mut ra = RunningAverage::new();
+        for i in 0..1000000 { // 1 BILLION iterations
+            ra.update(2.);
+        }
+        let (mean, var) = match ra.finalize(){
+            Some((m, v)) => (m, v),
+            None => {panic!("error, got None type from finalize during testing");}
+        };
+        println!("mean: {}, var: {}", mean, var);
+        assert_eq!(mean, 2.0);
+        assert_eq!(var, 0.0);
+    }
+
+    //future benchmarks should include things like:
+    //      vcfs with different number of sites (basically dense)
+    //      how frequent are reads containing mutations (reads with muts / reads)
+    //      how dense are reads containing mutations -- coverage (reads with muts / muts) 
+    //      different number of reads (does increasing bamsize but fixing other variables keep performance?)
     #[bench]
     fn iterate_bamvcf(b: &mut Bencher) {
         // we are supposed to provide a closure to b.iter() that does one step of 'work'
 
         println!("Starting benchmark test:");
         use rust_htslib::bam;
-        use rust_htslib::bam::record::CigarString;
         use std::path::Path;
 
         //prep bam
@@ -194,7 +481,7 @@ mod tests {
 
         //prep vcf
         use rust_htslib::bcf::Reader;
-        let vcf_path = Path::new("test/test1k.vcf");
+        let vcf_path = Path::new("test/test4k.vcf");
         let mut vcf_itr = Reader::from_path(vcf_path).expect("cannot open vcf"); //originally said .ok().unwrap() which seems straight stupid
         let mut vcf_list: Vec<rust_htslib::bcf::Record> = vec![];
         for entry in vcf_itr.records() {
@@ -202,8 +489,6 @@ mod tests {
         }
 
         //prep iterator
-        use rust_htslib::bam::record::Cigar;
-        use rust_htslib::bam::Read;
         let mut bamvcfrecord = BAMVCFRecord::new(&mut bam, vcf_list);
         let mut test = 0;
         b.iter( || {
@@ -216,3 +501,5 @@ mod tests {
         });
     }
 }
+
+
--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,27 @@ extern crate rust_htslib;
 use std::env;
 use std::str;
 use std::process;
+use std::collections::HashSet;
 use bamreadfilt::Config;
+use bamreadfilt::tuple_to_csv;
 use bamreadfilt::BAMVCFRecord;
-//use std::thread;
-//use std::time::Duration;
-//use byteorder::BigEndian;
-//use byteorder::ReadBytesExt;
-
-
-/*
-   IMPORTANT!!!!
-
- * Another thing to consider:
- * We have pileup capabilities within rust (ie given a set of reads at an alignment pileup variants  or some shit)
- *   we could look into using this to skip mpileup directly or to rewrite it using our tool.
- *   but that sounds hard -- consider it! benchmark it!
-
- * the next logical thing to do is see if we can take a bam, return it as an iterator (if it isnt already), and then
- *  write that bam to disk.
- * 
- * the EXTRA information we will ultimately need is: base/ref at position of variation, qual at position of variation, cigar at position of variation, mate pair?
- */
-
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    // we will need to configure our own setup here.
-    let config = Config::new(&args).unwrap_or_else(|err| {
-             println!("Problem parsing arguments: {}", err);
-             process::exit(1);
-    });
-    parse_bam(&config.bam, &config.vcf);
-
-    // cool so we can successfully parse the AD field now. what do we have left to do with this part?
-    //VCF gives us locations to go look for sites in the bam. next up is actually parsing the bam.
+    //parse_bam().filter() ?
+    //parse_bam(config.bam, config.vcf, config.filters) ?
+    /*match args {
+            filter statistics => populate struct of before/after reads/sites for a set of filters (basically call before and after applying filters and keep running counts/vecs)
+            collect read stats => populate struct of read-specific features (VBQ, MAPQ, DUP, ..etc ie what are the metrics of this sample?)
+            filter reads => just filters reads without any stats (but why would you want this)
+            make records => build tfrecords from the filters.
+      }
+    */
+    let config = Config::new();
+    parse_bam(config);
 }
 
-fn parse_bam(bamname: &str, vcf: &str){ 
+fn parse_bam(config: Config){ 
     /*
      * Accept a bamname, set of vcf sites and their alt-refs (just in a form that we can filter for quickly, maybe even a struct)
      *    returns an iterator of parsed bam entries that are contained in vcfinfo
@@ -54,54 +37,108 @@ fn parse_bam(bamname: &str, vcf: &str){
 
     use rust_htslib::bam;
     use std::path::Path;
-    let path = Path::new(bamname);
+
+    use rust_htslib::bcf::Reader;
+    use rust_htslib::bam::Read;
+
+    let path = Path::new(&config.bam_filename);
     let mut bam = bam::IndexedReader::from_path(path).ok().expect("Error opening bam.");
 
     let head = bam::Header::from_template(bam.header()); // wonder if i gotta clone dis;
-    let mut out = bam::Writer::from_path("test/out.bam", &head).expect("Error opening bam for writing.");
 
+    //need something here to determine between stdout (i think we can get a file handle for that) and an actual path.
+    //  ideally our config struct will handle this.
+
+    let mut out = match config.use_stdout {
+        false => bam::Writer::from_path("test/new_out.bam", &head).expect("Error opening bam for writing."), 
+        true => bam::Writer::from_stdout(&head).expect("Error opening stdout for writing."),
+    };
 
     // parse vcf -> vec
-    use rust_htslib::bcf::Reader;
-    let vcf_path = Path::new(vcf);
-    let mut vcf_itr = Reader::from_path(vcf_path).expect("cannot open vcf"); //originally said .ok().unwrap() which seems straight stupid
+    let vcf_path = Path::new(&config.vcf_filename);
+    let mut vcf_itr = Reader::from_path(vcf_path).expect("cannot open vcf"); 
     let mut vcf_list: Vec<rust_htslib::bcf::Record> = vec![];
     for entry in vcf_itr.records() {
         // TODO turn this into a match and explicitly handle failure cases (by exiting?)
+        //      I guess it just isnt clear to me how this fails.
         vcf_list.push(entry.ok().unwrap());
     }
 
-    // should probably figure out how to turn these into tests
-    println!("hello? guys?");
-    use rust_htslib::bam::Read;
-
-
     let bamvcfrecord = BAMVCFRecord::new(&mut bam, vcf_list);
 
-    // we constructed bamvcfrecord to only allow things that contained variants!
-    // but we need to designate a useful api for this
+    let mut before_stats = bamreadfilt::SiteStats::new();
+    let mut after_stats = bamreadfilt::SiteStats::new();
+
+    let mut before_site_stats: HashSet<bamreadfilt::VCFRecord> = HashSet::new();
+    let mut after_site_stats: HashSet<bamreadfilt::VCFRecord> = HashSet::new();
 
 
-    //DAMN this is fucking beautiful.
+    //DAMN this is fucking beautiful -- need to paramterize it though.
     for (i, (item, _vcf, _pir)) in bamvcfrecord
-                                        .filter(| &(ref _i, _v, p)| { p > 10 && p < 150 }) //pir filter
-                                        .filter(| &(ref i, _v, p) | {                     //vbq filter
-                                            i.qual()[p] >= 20                        
+                                        .map(   | (_i, _v, p) | { 
+                                            before_stats.collect_stats_stream(&_i, &_v, p);
+                                            before_site_stats.insert(_v.clone());
+                                            (_i, _v, p)
                                         })
-                                        .filter(| &(ref i, _v, _p)| { i.mapq() >= 40 })    //mapq filter
-                                        .filter(| &(ref i, _v, _p)| { bamreadfilt::mrbq(i) >= 30.0 }) //mrbq filter
+                                        .filter(| &(ref _i, _v, p)| { p >= config.min_pir && p <= config.max_pir}) //pir filter
+                                        .filter(| &(ref i, _v, p) | {                     //vbq filter
+                                            i.qual()[p] >= config.vbq
+                                        })
+                                        .filter(| &(ref i, _v, _p)| { i.mapq() >= config.mapq })    //mapq filter
+                                        .filter(| &(ref i, _v, _p)| { bamreadfilt::mrbq(i) >= config.mrbq }) //mrbq filter
+                                        .map(   | (_i, _v, p) | { 
+                                            after_stats.collect_stats_stream(&_i, &_v, p);
+                                            after_site_stats.insert(_v.clone());
+                                            (_i, _v, p)
+                                        })
                                         .enumerate() 
     {
-        // its just interesting to me that this is a filter!
-
-        if i % 1000 == 0 {
-            println!("{} reads processed.", i);
+        if config.verbose && i % 1000 == 0 {
+            eprintln!("{} reads processed.", i);
         }
-        //ultimately we will still have to do soething fancy here.
-        // also seems like this is a filter
         out.write(&item).unwrap(); // writes filtered reads.
     }
 
+    eprintln!("Number of sites before: {}, Number of sites after: {}", before_site_stats.len(), after_site_stats.len());
+
+
+    eprintln!("mrbq {:?} vbq {:?} mapq {:?} duplicate {:?} paired {:?}", before_stats.mrbq.finalize(), before_stats.vbq.finalize(), before_stats.mapq.finalize(), before_stats.duplicated.finalize(), before_stats.paired.finalize());
+    eprintln!("mrbq {:?} vbq {:?} mapq {:?} duplicate {:?} paired {:?}", after_stats.mrbq.finalize(), after_stats.vbq.finalize(), after_stats.mapq.finalize(), after_stats.duplicated.finalize(), after_stats.paired.finalize());
+    write_statistics(&"stats.txt".to_string(), before_stats, after_stats, before_site_stats.len() as u64, after_site_stats.len() as u64);
+}
+
+// probably want a result to type for if we ever error out.
+//  could be references and not moves/copies but we only do this once so whatever.
+//  need a way to include the before/after sites or something! maybe add it to the struct?
+pub fn write_statistics(output_filename: &String, before_stats: bamreadfilt::SiteStats, after_stats: bamreadfilt::SiteStats, before_sites: u64, after_sites: u64) -> Result<(),std::io::Error> {
+    use std::io::Write;
+    use std::fs::File;
+
+    let test_unpack = (1.0, 2.0);
+
+
+
+    let mut f = File::create(output_filename)?;
+    writeln!(&mut f, "setting,mrbq_mean,mrbq_var,vbq_mean,vbq_var,mapq_mean,mapq_var,duplicated_mean,duplicated_var,frag_size,num_sites,num_reads").unwrap();
+    writeln!(&mut f, "before,{},{},{},{},{},{},{}", 
+               tuple_to_csv(before_stats.mrbq.finalize().unwrap()), 
+               tuple_to_csv(before_stats.vbq.finalize().unwrap()), 
+               tuple_to_csv(before_stats.mapq.finalize().unwrap()), 
+               tuple_to_csv(before_stats.duplicated.finalize().unwrap()),
+               tuple_to_csv(before_stats.frag_size.finalize().unwrap()),
+               before_sites,
+               before_stats.num_reads
+           )?;
+    writeln!(&mut f, "after,{},{},{},{},{},{},{}", 
+               tuple_to_csv(after_stats.mrbq.finalize().unwrap()), 
+               tuple_to_csv(after_stats.vbq.finalize().unwrap()), 
+               tuple_to_csv(after_stats.mapq.finalize().unwrap()), 
+               tuple_to_csv(after_stats.duplicated.finalize().unwrap()),
+               tuple_to_csv(after_stats.frag_size.finalize().unwrap()),
+               after_sites,
+               after_stats.num_reads
+           )?;
+    Ok(())
 
 }
 
